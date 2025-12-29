467b950c202735a8f0e7560b9e6837b610d154ed
unix2dossss <vandan.ubuntu@gmail.com>
2025-12-29 22:20:08 +1300

    Setup recording

--- detection.py
+++ detection.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import numpy as np
 import cv2
 
@@ -10,6 +11,12 @@ class Detector:
         self.pixel_diff_thresh = pixel_diff_thresh
         self.area_thresh = area_thresh
 
+        self._detected_motion = False
+        self._recording_in_progress = False
+
+        self._frame_recording_count = 0
+        self._out = None
+
     def preprocess(self, frame, first=False):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -18,6 +25,24 @@ class Detector:
             self._prev_frame = blurred
 
         return blurred
+    
+    def setup_recording(self):
+        self._recording_in_progress = True
+
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+        self._out = cv2.VideoWriter(f'{timestamp}.avi', fourcc, 30, (640, 480))
+
+    def cleanup_recording(self):
+            self._detected_motion = False
+            self._recording_in_progress = False
+            self._frame_recording_count=0
+
+            self._out.release()
+            self._out = None
 
     def on_frame(self, frame):
         if self._prev_frame is None:
@@ -34,11 +59,24 @@ class Detector:
         motion_area = int(np.sum(mask > 0))
 
         if motion_area > self.area_thresh:
+            self._detected_motion = True
             print("Significant movement detected!")
             print("Changed pixels:", motion_area)
+
+        if self._detected_motion and not self._recording_in_progress:
+            print("recording started")
+            self.setup_recording()
+        
+        if self._recording_in_progress:
+            self._out.write(frame)
+            self._frame_recording_count += 1
 
         self._prev_frame = current_frame
 
         cv2.imshow("Preview", current_frame)
+
+        if self._frame_recording_count==10*30:
+            print("recording stopped")
+            self.cleanup_recording()
 
         cv2.waitKey(1)
