82ca66ec57505470e54340f7786193c78307c47d
unix2dossss <vandan.ubuntu@gmail.com>
2025-12-30 23:22:59 +0000

    Telegram Timeout Error handling

--- notification.py
+++ notification.py
@@ -12,20 +12,34 @@ if not BOT_TOKEN or not CHAT_ID:
 
 def notify(text: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    r = requests.post(url, data={"chat_id": CHAT_ID, "text": text}, timeout=10)
-    print(r.status_code, r.text)
+
+    try:
+        r = requests.post(url, data={"chat_id": CHAT_ID, "text": text}, timeout=10)
+        print(r.status_code, r.text)
+        r.raise_for_status()
+        return True
+    except requests.RequestException as e:
+        print(f"[WARN] Telegram notify failed: {e}")
+        return False
 
 
 def send_photo(image_path: str, caption: str = ""):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
-    with open(image_path, "rb") as f:
-        r = requests.post(
-            url,
-            data={"chat_id": CHAT_ID, "caption": caption},
-            files={"photo": f},
-            timeout=30
-        )
-    print("Telegram:", r.status_code, r.text)
+    try:
+        with open(image_path, "rb") as f:
+            r = requests.post(
+                url,
+                data={"chat_id": CHAT_ID, "caption": caption},
+                files={"photo": f},
+                timeout=30
+            )
+            r.raise_for_status()
+        print("Telegram:", r.status_code, r.text)
+        return True
+    except requests.RequestException as e:
+        print(f"[WARN] Telegram send photo failed: {e}")
+        return False
+
 
 if __name__ == "__main__":
     notify("notification.py test message")
